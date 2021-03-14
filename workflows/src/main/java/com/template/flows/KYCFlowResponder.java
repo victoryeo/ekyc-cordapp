5ba70a8d0ffd0099277beee9f7622cee36823b4c
victor yeo <victoryeo1974@gmail.com>
2021-03-14 14:35:44 +0800

    use idtest.txt as test data

--- workflows/src/main/java/com/template/flows/KYCFlowResponder.java
+++ workflows/src/main/java/com/template/flows/KYCFlowResponder.java
@@ -63,7 +63,7 @@ public class KYCFlowResponder extends FlowLogic<Void> {
 
         //Change the path to "../test.zip" for passing the unit test.
         //because the unit test are in a different working directory than the running node.
-        String zipPath = unitTest ? "../test.zip" : "../../../test.zip";
+        String zipPath = unitTest ? "../../../test.zip" : "../../../idtest.txt";
 
         SecureHash attachmentHash = null;
         try {
